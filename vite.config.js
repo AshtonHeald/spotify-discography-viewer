9580be0f0b743c28379cdc065a98258934af79f8
Ashton Heald <devtheash@gmail.com>
2024-03-25 11:47:35 -0500

    edited base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/spotify-artist-search/"
+  base: "/spotify-artist-search/",
+  plugins: [react()]
 })
