aac177efe888e23484bfcc0de924e47b300cfd73
Ashton Heald <devtheash@gmail.com>
2024-03-25 12:12:54 -0500

    edited deploy w/ routes

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,9 @@ import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router>
+    <Router 
+      basename={import.meta.env.DEV ? '/' : '/spotify-artist-search/'}
+      >
       <App />
     </Router>
   </React.StrictMode>,
